494c78fb23b92e8ee6b803604e8a11ff6834e945
Antoine de Mathelin <antoine.demat@gmail.com>
2020-06-25 10:14:09 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='adapt',
+    version='0.0.0',
+    description='Awesome Domain Adaptation Package Toolbox',
+    url='https://github.com/antoinedemathelin/adapt.git',
+    author='Antoine de Mathelin',
+    author_email='antoine.de_mathelin@cmla.ens-cachan.fr',
+    license='unlicense',
+    packages=['adapt'],
+    zip_safe=False
+)
