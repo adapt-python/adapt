2480bfd70beb426fe900c65aff5343f75951e2d2
antoinedemathelin <45264923+antoinedemathelin@users.noreply.github.com>
2020-07-03 15:23:34 +0200

    Update __init__.py

--- adapt/__init__.py
+++ adapt/__init__.py
@@ -1,6 +1,6 @@
-import feature_based
-import instance_based
-import parameter_based
-import utils
+# import feature_based
+# import instance_based
+# import parameter_based
+# import utils
 
 __all__ = ["feature_based", "instance_based", "parameter_based", "utils"]
