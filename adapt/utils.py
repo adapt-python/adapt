ff03fe4494f238f405142f247df62dd4c491640e
antoinedemathelin <antoine.demat@gmail.com>
2021-07-02 18:48:19 +0200

    add build in check network

--- adapt/utils.py
+++ adapt/utils.py
@@ -204,6 +204,7 @@ def check_network(network, copy=True,
             new_network = clone_model(network)
             if (hasattr(network, "input_shape") and
                 hasattr(new_network, "input_shape")):
+                new_network.build(input_shape=network.input_shape)
                 new_network.set_weights(network.get_weights())
         except Exception as e:
             if force_copy:
