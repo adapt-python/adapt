6aa41af87c79186d8369a3218de1eae001d87984
antoinedemathelin <antoine.demat@gmail.com>
2021-07-07 10:23:27 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='adaptation',
-    version='0.1.0',
-    description='Awesome Domain Adaptation Package Toolbox',
-    url='https://github.com/antoinedemathelin/adapt.git',
+    name='adapt',
+    version='0.2.0',
+    description='Awesome Domain Adaptation Package Toolbox for Tensorflow and Scikit-learn',
+    url='https://github.com/adapt-python/adapt.git',
     author='Antoine de Mathelin',
     author_email='antoine.de_mathelin@cmla.ens-cachan.fr',
     license='BSD-2',
