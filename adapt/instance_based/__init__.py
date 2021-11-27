985ce1b5a9a330658fa88f71777659a8b3417194
AnonymousAccount0 <antoine.de-mathelin-de-papigny@student.ecp.fr>
2021-11-27 16:16:09 +0100

    Add test WANN

--- adapt/instance_based/__init__.py
+++ adapt/instance_based/__init__.py
@@ -5,5 +5,6 @@ Instance-Based Methods Module
 from ._kliep import KLIEP
 from ._kmm import KMM
 from ._tradaboost import TrAdaBoost, TrAdaBoostR2, TwoStageTrAdaBoostR2
+from ._wann import WANN
 
-__all__ = ["KLIEP", "KMM", "TrAdaBoost", "TrAdaBoostR2", "TwoStageTrAdaBoostR2"]
+__all__ = ["KLIEP", "KMM", "TrAdaBoost", "TrAdaBoostR2", "TwoStageTrAdaBoostR2", "WANN"]
---
+++ tests/test_wann.py
@@ -0,0 +1,24 @@
+"""
+Test functions for wann module.
+"""
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from adapt.instance_based import WANN
+
+np.random.seed(0)
+Xs = np.concatenate((
+    np.random.randn(50)*0.1,
+    np.random.randn(50)*0.1 + 1.,
+)).reshape(-1, 1)
+Xt = (np.random.randn(100) * 0.1).reshape(-1, 1)
+ys = np.array([0.2 * x if x<0.5
+               else 10 for x in Xs.ravel()]).reshape(-1, 1)
+yt = np.array([0.2 * x if x<0.5
+               else 10 for x in Xt.ravel()]).reshape(-1, 1)
+
+def test_fit():
+    np.random.seed(0)
+    model = WANN(random_state=0)
+    model.fit(Xs, ys, Xt, yt, epochs=200, verbose=0)
+    assert np.abs(model.predict(Xt) - yt).sum() < 10
