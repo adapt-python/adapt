c5f6c03d61512f0baa98120db0ce05b0369b685c
antoinedemathelin <antoine.demat@gmail.com>
2022-02-10 14:52:23 +0100

    remove tests from package

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='adapt',
-    version='0.3.0',
+    version='0.3.1',
     description='Awesome Domain Adaptation Python Toolbox for Tensorflow and Scikit-learn',
     url='https://github.com/adapt-python/adapt.git',
     author='Antoine de Mathelin',
     author_email='antoine.demat@gmail.com',
     license='BSD-2',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     install_requires=["numpy>=1.16", "scipy>=1.0", "tensorflow>=2.0", "scikit-learn>=0.2", "cvxopt>=1.2"],
     zip_safe=False,
     long_description=long_description,
