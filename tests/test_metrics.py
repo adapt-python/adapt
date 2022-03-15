008c6d85e4238997c47f6ee9cb572355b02e46a5
antoinedemathelin <antoine.demat@gmail.com>
2022-03-15 14:23:22 +0100

    Hide KMM test from windows

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,6 +2,7 @@
 Test base
 """
 
+import os
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.model_selection import GridSearchCV
 from adapt.base import BaseAdaptDeep, BaseAdaptEstimator
@@ -34,19 +35,20 @@ def test_all_metrics():
     
     
 def test_adapt_scorer():
-    scorer = make_uda_scorer(j_score, Xs, Xt)
-    adapt_model = KMM(LinearRegression(), Xt=Xt, kernel="rbf", gamma=0.)
-    gs = GridSearchCV(adapt_model, {"gamma": [1000, 1e-5]},
-                      scoring=scorer, return_train_score=True,
-                      cv=3, verbose=0, refit=False)
-    gs.fit(Xs, ys)
-    assert gs.cv_results_['mean_train_score'].argmax() == 0
-    
-    scorer = make_uda_scorer(cov_distance, Xs, Xt)
-    adapt_model = CORAL(LinearRegression(), Xt=Xt, lambda_=1.)
-    gs = GridSearchCV(adapt_model, {"lambda_": [1e-5, 10000.]},
-                      scoring=scorer, return_train_score=True,
-                      cv=3, verbose=0, refit=False)
-    gs.fit(Xs, ys)
-    assert gs.cv_results_['mean_train_score'].argmax() == 0
-    assert gs.cv_results_['mean_test_score'].argmax() == 0
+    if os.name != 'nt':
+        scorer = make_uda_scorer(j_score, Xs, Xt)
+        adapt_model = KMM(LinearRegression(), Xt=Xt, kernel="rbf", gamma=0.)
+        gs = GridSearchCV(adapt_model, {"gamma": [1000, 1e-5]},
+                          scoring=scorer, return_train_score=True,
+                          cv=3, verbose=0, refit=False)
+        gs.fit(Xs, ys)
+        assert gs.cv_results_['mean_train_score'].argmax() == 0
+
+        scorer = make_uda_scorer(cov_distance, Xs, Xt)
+        adapt_model = CORAL(LinearRegression(), Xt=Xt, lambda_=1.)
+        gs = GridSearchCV(adapt_model, {"lambda_": [1e-5, 10000.]},
+                          scoring=scorer, return_train_score=True,
+                          cv=3, verbose=0, refit=False)
+        gs.fit(Xs, ys)
+        assert gs.cv_results_['mean_train_score'].argmax() == 0
+        assert gs.cv_results_['mean_test_score'].argmax() == 0
