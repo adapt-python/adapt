58932c43320d35fce73db6e6e8bb94df1a3306bd
antoinedemathelin <antoine.demat@gmail.com>
2022-03-27 16:20:11 +0200

    change pairwise distance

--- adapt/feature_based/_ccsa.py
+++ adapt/feature_based/_ccsa.py
@@ -17,15 +17,12 @@ def pairwise_y(X, Y):
     Y = tf.tile(tf.expand_dims(Y, -1), [1, 1, batch_size_x])
     return tf.reduce_sum(tf.abs(X-tf.transpose(Y)), 1)/2
 
+
 def pairwise_X(X, Y):
-    batch_size_x = tf.shape(X)[0]
-    batch_size_y = tf.shape(Y)[0]
-    dim = tf.reduce_prod(tf.shape(X)[1:])
-    X = tf.reshape(X, (batch_size_x, dim))
-    Y = tf.reshape(Y, (batch_size_y, dim))
-    X = tf.tile(tf.expand_dims(X, -1), [1, 1, batch_size_y])
-    Y = tf.tile(tf.expand_dims(Y, -1), [1, 1, batch_size_x])
-    return tf.reduce_sum(tf.square(X-tf.transpose(Y)), 1)
+    X2 = tf.tile(tf.reduce_sum(tf.square(X), axis=1, keepdims=True), [1, tf.shape(Y)[0]])
+    Y2 = tf.tile(tf.reduce_sum(tf.square(Y), axis=1, keepdims=True), [1, tf.shape(X)[0]])
+    XY = tf.matmul(X, tf.transpose(Y))
+    return X2 + tf.transpose(Y2) - 2*XY
 
 
 @make_insert_doc(["encoder", "task"], supervised=True)
