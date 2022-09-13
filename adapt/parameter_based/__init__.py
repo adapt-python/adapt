492e59eda66a1a50cc262421b2e1628aa7986ae7
antoinedemathelin <antoine.demat@gmail.com>
2022-09-13 15:00:51 +0200

    Add LinInt

--- adapt/parameter_based/__init__.py
+++ adapt/parameter_based/__init__.py
@@ -6,10 +6,12 @@ from ._regular import RegularTransferLR, RegularTransferLC, RegularTransferNN
 from ._finetuning import FineTuning
 from ._transfer_tree import TransferTreeClassifier
 from ._transfer_tree import TransferForestClassifier
+from ._linint import LinInt
 
 __all__ = ["RegularTransferLR",
            "RegularTransferLC",
            "RegularTransferNN",
            "FineTuning",
            "TransferTreeClassifier",
-           "TransferForestClassifier"]
+           "TransferForestClassifier",
+           "LinInt"]
